55b046ec13d2f1c4788c1e7a597c0e2459909359
Diego Managlia <diego.managlia@gmail.com>
2024-08-16 11:39:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React SecretWord Game
+
+This project was made with React JS for practice.
+
+Link to this project live: 
---
+++ src/components/GameOver.js
@@ -0,0 +1,12 @@
+import "./GameOver.css";
+
+const GameOver = ({ retry, score }) => {
+    return <div>
+        <h1>Fim de Jogo!</h1>
+        <h2>A sua pontuação foi de: <span>{score}</span></h2>
+
+        <button onClick={retry}>Reiniciar o Jogo</button>
+    </div>
+};
+
+export default GameOver;
